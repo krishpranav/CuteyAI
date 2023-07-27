8ed53df0a23e262897ed71f4c5335493cbdf32e5
krishpranav <krisna.pranav@gmail.com>
2023-07-27 21:25:30 +0530

    types: index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,38 +1,42 @@
-export enum PresentType {
+export interface PluginType {
+    logo: string
+    id: PresetType
+    title: PresetType
+    desc?: string
+    loading: boolean
+    inputDisable?: boolean
+    nostore?: boolean
+    monitorClipboard?: boolean
+    monitorBrowser?: boolean
+}
+
+export interface PresetModule {
+    listVisible: boolean
+    builtInPlugins: PluginType[]
+    currentPreset: PresetType
+}
+  
+export enum PresetType {
     Casual = 'Casual',
     Translator = 'Translator',
     Summarizer = 'Summarizer',
     Programmer = 'Programmer',
-    Analyst = 'Analyst'
+    Analyst = 'Analyst',
 }
-
+  
 export interface PanelVisible {
     plugin?: boolean
     setting?: boolean
     chatPanel?: boolean
 }
-
+  
 export interface DataType {
     key: string
     character: string
     prompt: string
 }
-
+  
 export interface PosType {
     posX: number
     posY: number
-}
-
-export interface PresentModule {
-    listVisible: boolean
-    builtInPlugins: PluginType[]
-    currentPresent: PresentType
-}
-
-export interface PluginType {
-    logo: string
-    id: PresentType
-    title: PresentType
-    desc?: string
-    loading: boolean
 }
